89b2830804786b2cd9e8afa1cffd265f10ea1542
heweizhi <heweizhi@guanmai.cn>
2021-04-13 11:46:30 +0800

    feat: pro

---
+++ script/webpack.config.js
@@ -0,0 +1,64 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path')
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(css|less)$/,
+        use: [
+            {
+                loader: "style-loader",
+            },
+            {
+                loader: "css-loader",
+                options: {
+                    importLoaders: 1,
+                },
+            },
+            {
+                loader: "less-loader",
+                options: {
+                    javascriptEnabled: true,
+                },
+            },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|jpeg)$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        loader: "file-loader"
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'pro',
+      template: 'dist/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    hot: true,
+    historyApiFallback: true,
+    compress: true,
+  }
+}
